ee64e59302a36c92d65aa0667586ca6cc7b04972
adworetzky <adworetzky@mica.edu>
2022-02-11 00:31:48 -0500

    comments

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // Canvas size variables
 var exportWidth = 1220;
 var exportHeight = 723;
+
 var stockImg = new Image(); // Create new img element
 stockImg.src = "stock img/placeholderimg.png";
 var maskImg = new Image(); // Create new img element
 maskImg.src = "mask/Mask.png";
 var meshImg = new Image(); // Create new img element
 
+// fill and array with src links to mesh files
 let imgArray = [];
 for (let index = 0; index <= 49; index++) {
   imgArray[index] = "mesh/mesh" + index + ".png";
 }
+
+// set initial mesh to display
 meshImg.src = imgArray[7];
+
 // Dynamic element creation
 // Containers
 const main = document.querySelector("main");
@@ -27,6 +32,7 @@ main.append(c);
 c.setAttribute("id", "canvas");
 c.height = exportHeight;
 c.width = exportWidth;
+
 const c1 = document.createElement("canvas");
 c1.setAttribute("id", "canvas1");
 c1.height = exportHeight;
@@ -37,6 +43,7 @@ uiContainer.append(fileInput);
 fileInput.classList.add("uiElement");
 fileInput.type = "file";
 fileInput.setAttribute("id", "fileUpload");
+
 const fileInputLabel = document.createElement("label");
 fileInputLabel.classList.add("uiElementLabel");
 fileInputLabel.setAttribute("for", "fileUpload");
@@ -47,6 +54,7 @@ const meshSelect = document.createElement("select");
 uiContainer.append(meshSelect);
 meshSelect.classList.add("uiElement");
 meshSelect.setAttribute("id", "meshSelect");
+
 const meshSelectLabel = document.createElement("label");
 meshSelectLabel.classList.add("uiElementLabel");
 meshSelectLabel.setAttribute("for", "meshSelect");
@@ -58,6 +66,9 @@ for (var i = 0; i < imgArray.length; i++) {
   opt.innerHTML = "Mesh:" + (i + 1);
   meshSelect.appendChild(opt);
 }
+window.onload = function () {
+  document.getElementById("meshSelect").focus();
+};
 
 // load image validation
 stockImg.addEventListener(
@@ -91,32 +102,37 @@ maskImg.addEventListener(
   false
 );
 
-// Function to scale to fill image, draw img to canvas // Main draw function
-
+// function to handle drawing mesh to c1 canvas
 function drawMesh(img, canvas) {
   console.log("draw function trigger");
 
   let ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // draw mask
   ctx.globalCompositeOperation = "source-over";
   ctx.drawImage(maskImg, 0, 0, canvas.width, canvas.height);
-  ctx.globalCompositeOperation = "source-in";
 
+  // draw mesh inside mask
+  ctx.globalCompositeOperation = "source-in";
   ctx.drawImage(img, -5, -5, canvas.width + 10, canvas.height + 10);
-  // ctx.globalCompositeOperation = "multiply";
-  // ctx.drawImage(img, -5, -5, canvas.width + 10, canvas.height + 10);
 }
+
+// function to take c1 canvas and draw it overtop of c canvas
 function drawImg(img, canvas) {
   let ctx = canvas.getContext("2d");
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   // get the scale
   var scale = Math.max(canvas.width / img.width, canvas.height / img.height);
   // get the top left position of the image
   var x = canvas.width / 2 - (img.width / 2) * scale;
   var y = canvas.height / 2 - (img.height / 2) * scale;
 
+  // draw stock image
   ctx.globalCompositeOperation = "source-over";
   ctx.drawImage(img, x, y, (img.width + 1) * scale, (img.height + 1) * scale);
+
+  //draw mesh (previouslymasked)
   ctx.globalCompositeOperation = "color";
   ctx.drawImage(c1, x, y, (img.width + 1) * scale, (img.height + 1) * scale);
   ctx.globalCompositeOperation = "multiply";
@@ -124,6 +140,7 @@ function drawImg(img, canvas) {
   ctx.drawImage(c1, x, y, (img.width + 1) * scale, (img.height + 1) * scale);
 }
 
+// redraw canvas on image upload event
 fileInput.addEventListener("change", function () {
   console.log(fileInput.files[0]);
   stockImg.src = URL.createObjectURL(fileInput.files[0]);
@@ -131,6 +148,7 @@ fileInput.addEventListener("change", function () {
   drawImg(stockImg, c);
 });
 
+// redraw canvas on mesh select event
 meshSelect.addEventListener("change", function () {
   meshImg.src = meshSelect.value;
 
